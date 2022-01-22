b63eecdf679c9ed401a7a211e3f1fb56953e7e2c
Kalle Kromann <kallekro@gmail.com>
2022-01-22 09:14:17 +0100

    Remove names from word database

--- getwords.py
+++ getwords.py
@@ -1,9 +1,9 @@
 words = []
 with open("/usr/share/dict/words", "r") as fd:
-    for line in fd:
+    for i, line in enumerate(fd):
         word = line.strip()
-        if len(word) == 5:
+        if len(word) == 5 and not ord(word[0]) < 91:
             words.append(word.upper())
 
 with open('data.js', 'w') as fd:
-    fd.write("const words = %s;\nexport default words;" % repr(words))
+    fd.write("const words = %s;\nexport {words};" % repr(words))
